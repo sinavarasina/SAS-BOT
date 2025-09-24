2c2cd190ddf3a1650ddf0fbacb9649d124016fca
sinavarasina <varasinafarmadani@gmail.com>
2025-09-25 03:20:07 +0700

    bot base commit

---
+++ internal/db/schema.go
@@ -0,0 +1,24 @@
+package db
+
+import (
+	"github.com/jmoiron/sqlx"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func InitDB(path string) (*sqlx.DB, error) {
+	db, err := sqlx.Open("sqlite3", path)
+	if err != nil {
+		return nil, err
+	}
+
+	schema := `
+	CREATE TABLE IF NOT EXISTS users (
+		jid TEXT PRIMARY KEY,
+		number TEXT,
+		username TEXT,
+		updated_at DATETIME
+	);
+	`
+	_, err = db.Exec(schema)
+	return db, err
+}
