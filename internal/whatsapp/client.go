222eff7dc13bf4fb3b2e87edd596c1497f0c6bd3
sinavarasina <varasinafarmadani@gmail.com>
2025-10-06 19:29:38 +0700

    try implement postgres atleast at client

--- internal/whatsapp/client.go
+++ internal/whatsapp/client.go
@@ -6,6 +6,7 @@ import (
 	"os"
 
 	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
 	"github.com/mdp/qrterminal"
 	"go.mau.fi/whatsmeow"
 	"go.mau.fi/whatsmeow/store/sqlstore"
@@ -14,7 +15,8 @@ import (
 
 func InitClient(appDB *sqlx.DB, ctx context.Context) (*whatsmeow.Client, error) {
 	dbLog := waLog.Stdout("Database", "INFO", true)
-	container, err := sqlstore.New(ctx, "sqlite3", "file:wa_session.db?_foreign_keys=on", dbLog)
+	dsn := "postgres://avnadmin:AVNS_ufUAE5DXZUEYoiPu8-1@sasdb-student-5e9a.e.aivencloud.com:21599/defaultdb?sslmode=require"
+	container, err := sqlstore.New(ctx, "postgres", dsn, dbLog)
 	if err != nil {
 		return nil, err
 	}
