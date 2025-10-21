be17356d7d8e69825d296fc7e4e782b7fa6de23a
dnielclv4free <danielcalvin822@gmail.com>
2025-10-21 14:49:50 +0700

    feat pengaduan

--- internal/db/schema.go
+++ internal/db/schema.go
@@ -18,6 +18,13 @@ func InitDB(dsn string) (*sqlx.DB, error) {
 		username TEXT,
 		previlege TEXT
 	);
+	CREATE TABLE IF NOT EXISTS pengaduan (
+		id SERIAL PRIMARY KEY,
+		user_jid TEXT,
+		deskripsi TEXT,
+		pict_path TEXT,
+		sent_time TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+	);
 	`
 	_, err = db.Exec(schema)
 	return db, err
