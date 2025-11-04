71aa6477d2e889ca5c4bce0cfafa6dadbbe90ff3
sinavarasina <varasinafarmadani@gmail.com>
2025-11-04 12:25:26 +0700

    add comment for future development

--- internal/bot/data_entry_compat.go
+++ internal/bot/data_entry_compat.go
@@ -6,6 +6,8 @@ import (
 	"github.com/sinavarasina/SAS-BOT/internal/sheets"
 )
 
+// fungsi abstraksi (untuk layer kompatibilitas sementara)
+
 func HandleDataEntryCompat(dbConn *sqlx.DB, jid, text string, session *db.DataEntrySession, sheetsClient *sheets.SheetsClient) []string {
 	return HandleDataEntry(dbConn, jid, text, session, sheetsClient)
 }
