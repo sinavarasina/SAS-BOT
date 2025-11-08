8efc26b3e8840d456552ca502f57e8b503c7ad40
sinavarasina <varasinafarmadani@gmail.com>
2025-11-09 06:17:54 +0700

    fix bug, and some enhancement

--- internal/surat/model.go
+++ internal/surat/model.go
@@ -10,20 +10,78 @@ const (
 	KEMATIAN        JenisSurat = "sk_kematian.tex"
 )
 
-var BaseFields = []string{
-	"NAMA",
-	"TTL",
-	"JK",
-	"AGAMA",
-	"NIK",
-	"PEKERJAAN",
-	"ALAMAT",
-}
+//
+// var BaseFields = []string{
+// 	"NAMA",
+// 	"TTL",
+// 	"JK",
+// 	"AGAMA",
+// 	"NIK",
+// 	"PEKERJAAN",
+// 	"ALAMAT",
+// }
+
+// i think to reduce complexity, i just make it specific.
 
 var SuratFields = map[JenisSurat][]string{
-	DOMISILI:        {"ALASANPERLU"},
-	USAHA:           {"TTLnU", "ALAMATDOM", "DUSUN"},
-	SKTM_UMUM:       {"TTLnU", "ALASANPERLU"},
-	SKTM_TANGGUNGAN: {"NAMA.P", "JK.P", "TTL.P", "NIK.P", "PEKERJAAN.P", "AGAMA.P", "STATUS.P", "ALAMAT.P", "NAMA.C", "JK.C", "TTL.C", "NIK.C", "PEKERJAAN.C", "AGAMA.C", "ALAMAT.C", "ALASANPERLU"},
-	KEMATIAN:        {"FNAMAnAL", "BINoBINTI", "TTLnU", "AGAMA", "PERKERJAAN", "POSISITERAKHIR", "HARI", "TGL", "JAM", "TEMPAT", "ALASANMENINGGAL"},
+	DOMISILI: {
+		"NAMA",
+		"TTL",
+		"JK",
+		"AGAMA",
+		"NIK",
+		"PEKERJAAN",
+		"ALAMAT",
+		"ALASANPERLU",
+	},
+	USAHA: {
+		"NAMA",
+		"JK",
+		"TTLnU",
+		"NIK",
+		"AGAMA",
+		"PEKERJAAN",
+		"ALAMAT",
+		"ALAMATDOM",
+		"DUSUN",
+	},
+	SKTM_UMUM: {
+		"NAMA",
+		"TTLnU",
+		"NIK",
+		"PEKERJAAN",
+		"ALAMAT",
+		"ALASANPERLU",
+	},
+	SKTM_TANGGUNGAN: {
+		"NAMA.P", // fyi it might be weird,
+		"JK.P",   // but i have no idea to naming it
+		"TTL.P",  // so i choose .P as Parent, .C as Child.
+		"NIK.P",  // stupid but yea just change it if you dont like
+		"PEKERJAAN.P",
+		"AGAMA.P",
+		"STATUS.P",
+		"ALAMAT.P",
+		"NAMA.C",
+		"JK.C",
+		"TTL.C",
+		"NIK.C",
+		"PEKERJAAN.C",
+		"AGAMA.C",
+		"ALAMAT.C",
+		"ALASANPERLU",
+	},
+	KEMATIAN: {
+		"FNAMAnAL",
+		"BINoBINTI",
+		"TTLnU",
+		"AGAMA",
+		"PEKERJAAN",
+		"POSISITERAKHIR",
+		"HARI",
+		"TGL",
+		"JAM",
+		"TEMPAT",
+		"ALASANMENINGGAL",
+	},
 }
